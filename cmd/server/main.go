8d6ea02e18bf174d4f7a8c5193be9855c12461a9
hanklin <hanklin21000@gmail.com>
2024-11-04 02:13:14 +0800

    feat: add flag and task build command

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"flag"
+	"time"
+
 	"github.com/HankLin216/go-utils/config"
 	"github.com/HankLin216/go-utils/config/file"
 	"github.com/HankLin216/grpc-boilerplate/internal/conf"
 	"go.uber.org/zap"
 )
 
+var (
+	Name           = "grpc-boilerplate"
+	Version        = "v1.0.0"
+	Env            = "Development"
+	ConfFolderPath = "../../configs"
+	BuildTime      = time.Now().Format(time.RFC3339)
+)
+
+func init() {
+	flag.StringVar(&Version, "version", Version, "input the service version")
+	flag.StringVar(&Env, "env", Env, "input the environment")
+	flag.StringVar(&ConfFolderPath, "ConfFolderPath", ConfFolderPath, "input the config path")
+}
+
 func main() {
-	logger, err := zap.NewDevelopment()
+	// flag
+	flag.Parse()
+
+	// logger
+	var logger *zap.Logger
+	var err error
+	if Env == "Production" {
+		logger, err = zap.NewProduction()
+	} else {
+		logger, err = zap.NewDevelopment()
+	}
 	if err != nil {
 		panic(err)
 	}
 	defer logger.Sync()
+	logger.Info("Server infos",
+		zap.String("Name", Name),
+		zap.String("Version", Version),
+		zap.String("Env", Env),
+		zap.String("ConfigFolderPath", ConfFolderPath),
+		zap.String("BuildTime", BuildTime),
+	)
 
 	// config
 	c := config.New(
 		logger,
 		config.WithSource(
-			file.NewSource("../../configs"),
+			file.NewSource(ConfFolderPath),
 		),
 	)
 	defer c.Close()
@@ -31,14 +65,4 @@ func main() {
 	if err := c.Scan(&bc); err != nil {
 		panic(err)
 	}
-
-	// some log
-	logger.Debug("this is debug message")
-	logger.Info("this is info message")
-	logger.Info("this is info message with fileds",
-		zap.Int("age", 37),
-		zap.String("agender", "man"),
-	)
-	logger.Warn("this is warn message")
-	logger.Error("this is error message")
 }
